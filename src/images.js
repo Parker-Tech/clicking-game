469ef39878d8b3c7503e8438e6111fba24ac741b
JP <parker.tech9@gmail.com>
2019-08-01 14:44:27 -0400

    added base information but trying to fix the render

---
+++ src/images.js
@@ -0,0 +1,29 @@
+import ang from './assets/ang.png'
+import appa from './assets/appa.png'
+import azula from './assets/azula.png'
+import boomy from './assets/boomy.png'
+import firelord from './assets/firelord.png'
+// import iroh from './assets/iroh.png'
+import katara from './assets/katara.png'
+import mai from './assets/mai.png'
+import momo from './assets/momo.png'
+import zuko from './assets/zuko.png'
+import toph from './assets/toph.png'
+import sukki from './assets/sukki.png'
+
+const images = [
+  ang,
+  appa,
+  azula,
+  boomy,
+  firelord,
+  // iroh,
+  katara,
+  mai,
+  momo,
+  zuko,
+  toph,
+  sukki
+]
+
+export default images
